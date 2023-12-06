8af3c344626b01c5c33ea931254e8b51d54c90de
Fredrik Haarde <fredrik.haarde@bouvet.no>
2023-12-06 14:05:38 +0100

    Init solution and project for 2023

---
+++ 2023/Day1/Day1.cs
@@ -0,0 +1,28 @@
+namespace _2023.Day1
+{
+    public class Day1
+    {
+        private static readonly string[] input = File.ReadAllLines("./input.txt");
+
+        [Fact]
+        public void PartOne()
+        {
+            int result = 0;
+
+            foreach (string line in input)
+            {
+
+            }
+
+            Assert.True(result == -1);
+        }
+
+        [Fact]
+        public void PartTwo()
+        {
+            int result = 0;
+
+            Assert.True(result == -1);
+        }
+    }
+}
