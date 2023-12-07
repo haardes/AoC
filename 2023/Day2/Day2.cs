2827ac46208e5b3a1d73e0b55fc20fb4d61065d7
Fredrik Haarde <fredrik.haarde@bergen.kommune.no>
2023-12-07 13:46:04 +0100

    Day 2

--- 2023/Day2/Day2.cs
+++ 2023/Day2/Day2.cs
@@ -1,6 +1,7 @@
+
 namespace _2023.Day2;
 
-public class Day1 
+public class Day2
 {
     private static readonly string[] input = File.ReadAllLines("../../../Day2/input.txt");
 
@@ -9,8 +10,67 @@ public class Day1
     {
         int result = 0;
 
+        int maxRed = 12;
+        int maxGreen = 13;
+        int maxBlue = 14;
+
+        foreach (string line in input)
+        {
+            int gameId = int.Parse(line.Split(":").First().Split(" ").Last());
+            string gameString = line.Split(":").Last();
+            bool validGame = true;
+
+            string[] sets = gameString.Split(";");
+
+            foreach (string set in sets)
+            {
+                if (!validGame)
+                {
+                    break;
+                }
+
+                string[] cubeStrings = set.Split(",");
+                foreach (string cubeString in cubeStrings)
+                {
+                    int count = int.Parse(cubeString.Trim().Split(" ").First());
+                    string color = cubeString.Trim().Split(" ").Last();
+
+                    if (color == "red")
+                    {
+                        if (count > maxRed)
+                        {
+                            validGame = false;
+                            break;
+                        }
+                    }
+
+                    if (color == "green")
+                    {
+                        if (count > maxGreen)
+                        {
+                            validGame = false;
+                            continue;
+                        }
+                    }
+
+                    if (color == "blue")
+                    {
+                        if (count > maxBlue)
+                        {
+                            validGame = false;
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            if (!validGame) continue;
+
+            result += gameId;
+        }
+
         Console.WriteLine(result);
-        Assert.Equal(-1, result);
+        Assert.Equal(3059, result);
     }
 
     [Fact]
@@ -18,7 +78,49 @@ public class Day1
     {
         int result = 0;
 
+        foreach (string line in input)
+        {
+            string gameString = line.Split(":").Last();
+
+            result += CalculatePowerForGame(gameString);
+        }
+
         Console.WriteLine(result);
-        Assert.Equal(-1, result);
+        Assert.Equal(65371, result);
+    }
+
+    private static int CalculatePowerForGame(string gameString)
+    {
+        string[] sets = gameString.Split(";");
+        int maxRed = 0;
+        int maxGreen = 0;
+        int maxBlue = 0;
+
+        foreach (string set in sets)
+        {
+            string[] cubeStrings = set.Split(",");
+            foreach (string cubeString in cubeStrings)
+            {
+                int count = int.Parse(cubeString.Trim().Split(" ").First());
+                string color = cubeString.Trim().Split(" ").Last();
+
+                if (color == "red" && count > maxRed)
+                {
+                    maxRed = count;
+                }
+
+                if (color == "green" && count > maxGreen)
+                {
+                    maxGreen = count;
+                }
+
+                if (color == "blue" && count > maxBlue)
+                {
+                    maxBlue = count;
+                }
+            }
+        }
+
+        return maxRed * maxGreen * maxBlue;
     }
 }
