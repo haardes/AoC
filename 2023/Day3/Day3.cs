d6b20cd9b44014f83059be0f5e26187cd72e9c21
Fredrik Haarde <fredrik.haarde@bergen.kommune.no>
2023-12-07 14:38:47 +0100

    Day3:Part1

--- 2023/Day3/Day3.cs
+++ 2023/Day3/Day3.cs
@@ -1,6 +1,6 @@
 namespace _2023.Day3;
 
-public class Day1 
+public class Day3
 {
     private static readonly string[] input = File.ReadAllLines("../../../Day3/input.txt");
 
@@ -8,6 +8,82 @@ public class Day1
     public void PartOne()
     {
         int result = 0;
+
+        for (var x = 0; x < input.Length; x++)
+        {
+            for (var y = 0; y < input[x].Length; y++)
+            {
+                char cell = input[x][y];
+
+                if (!char.IsDigit(cell))
+                {
+                    continue;
+                }
+
+                bool isValidPartNumber = false;
+                string number = "";
+
+                // Left
+                if (y > 0)
+                {
+                    isValidPartNumber = !input[x][y - 1].Equals('.');
+                }
+
+                // Left above
+                if (y > 0 && x > 0)
+                {
+                    isValidPartNumber = !input[x - 1][y - 1].Equals('.');
+                }
+
+                // Left below
+                if (y > 0 && x < input.Length - 1)
+                {
+                    isValidPartNumber = !input[x + 1][y - 1].Equals('.');
+                }
+
+                // Above
+                if (x > 0)
+                {
+                    isValidPartNumber = !input[x - 1][y].Equals('.');
+                }
+
+                // Below
+                if (x < input.Length - 1)
+                {
+                    isValidPartNumber = !input[x + 1][y].Equals('.');
+                }
+
+                while (char.IsDigit(input[x][y]))
+                {
+                    number += input[x][y];
+
+                    // Right above
+                    if (x > 0 && y < input[x].Length - 1)
+                    {
+                        isValidPartNumber = !input[x - 1][y + 1].Equals('.');
+                    }
+
+                    // Right below
+                    if (x < input.Length - 1 && y < input[x].Length - 1)
+                    {
+                        isValidPartNumber = !input[x + 1][y + 1].Equals('.');
+                    }
+
+                    y++;
+                }
+
+                // Right
+                if (y < input[x].Length - 1)
+                {
+                    isValidPartNumber = !input[x][y + 1].Equals('.');
+                }
+
+                if (isValidPartNumber)
+                {
+                    result += int.Parse(number);
+                }
+            }
+        }
 
         Console.WriteLine(result);
         Assert.Equal(-1, result);
