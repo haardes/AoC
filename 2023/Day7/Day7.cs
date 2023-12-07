1257b6f8d15ff3559e3456c646c2e2f7e8991a11
Fredrik Haarde <fredrik.haarde@bergen.kommune.no>
2023-12-07 12:02:09 +0100

    Ran template-file

---
+++ 2023/Day7/Day7.cs
@@ -0,0 +1,24 @@
+namespace _2023.Day7;
+
+public class Day1 
+{
+    private static readonly string[] input = File.ReadAllLines("../../../Day7/input.txt");
+
+    [Fact]
+    public void PartOne()
+    {
+        int result = 0;
+
+        Console.WriteLine(result);
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void PartTwo()
+    {
+        int result = 0;
+
+        Console.WriteLine(result);
+        Assert.Equal(-1, result);
+    }
+}
---
+++ 2023/Day9/Day9.cs
@@ -0,0 +1,24 @@
+namespace _2023.Day9;
+
+public class Day1 
+{
+    private static readonly string[] input = File.ReadAllLines("../../../Day9/input.txt");
+
+    [Fact]
+    public void PartOne()
+    {
+        int result = 0;
+
+        Console.WriteLine(result);
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void PartTwo()
+    {
+        int result = 0;
+
+        Console.WriteLine(result);
+        Assert.Equal(-1, result);
+    }
+}
