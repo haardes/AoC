9f3c1efcd585f0dc0a75ec931cd392ffb703a368
Fredrik Haarde <fredrik.haarde@bergen.kommune.no>
2023-12-11 13:45:35 +0100

    Day 9

--- 2023/Day9/Day9.cs
+++ 2023/Day9/Day9.cs
@@ -9,8 +9,80 @@ public class Day09
     {
         int result = 0;
 
+        foreach (var history in input)
+        {
+            var values = history.Split(" ").Select(value => int.Parse(value)).ToList();
+            Reducer reducer = new(values);
+
+            while (reducer.CanReduce())
+            {
+                reducer.Reduce();
+            }
+
+            result += reducer.PredictForward();
+        }
+
         Console.WriteLine(result);
-        Assert.Equal(-1, result);
+        Assert.Equal(2101499000, result);
+    }
+
+    private class Reducer
+    {
+        public readonly List<int> Values;
+        private readonly List<List<int>> _history = new();
+
+
+        public Reducer(List<int> ints)
+        {
+            Values = ints;
+            _history.Add(Values);
+        }
+
+        public void Reduce()
+        {
+            var values = _history.Last();
+            var result = new List<int>();
+
+            for (var i = 0; i < values.Count - 1; i++)
+            {
+                result.Add(values[i + 1] - values[i]);
+            }
+
+            _history.Add(result);
+        }
+
+        public bool CanReduce()
+        {
+            return !_history.Last().All(value => value == 0);
+        }
+
+        public int PredictForward()
+        {
+            for (var i = _history.Count - 1; i >= 0; i--)
+            {
+                if (i == _history.Count - 1) _history[i].Add(0);
+                else
+                {
+                    _history[i].Add(_history[i].Last() + _history[i + 1].Last());
+                }
+            }
+
+            return _history.First().Last();
+        }
+
+        public int PredictBackward()
+        {
+            for (var i = _history.Count - 1; i >= 0; i--)
+            {
+                if (i == _history.Count - 1) _history[i].Insert(0, 0);
+                else
+                {
+                    _history[i].Insert(0, _history[i].First() - _history[i + 1].First());
+                }
+            }
+
+            return _history.First().First();
+        }
     }
 
     [Fact]
@@ -18,7 +90,20 @@ public class Day09
     {
         int result = 0;
 
+        foreach (var history in input)
+        {
+            var values = history.Split(" ").Select(value => int.Parse(value)).ToList();
+            Reducer reducer = new(values);
+
+            while (reducer.CanReduce())
+            {
+                reducer.Reduce();
+            }
+
+            result += reducer.PredictBackward();
+        }
+
         Console.WriteLine(result);
-        Assert.Equal(-1, result);
+        Assert.Equal(1089, result);
     }
 }
