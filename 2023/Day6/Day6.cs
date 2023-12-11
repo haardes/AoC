3e13e9ed5317f074609f5a397e6ee373ba7f6a67
Fredrik Haarde <fredrik.haarde@bergen.kommune.no>
2023-12-11 11:23:21 +0100

    Day 6

--- 2023/Day6/Day6.cs
+++ 2023/Day6/Day6.cs
@@ -7,18 +7,56 @@ public class Day06
     [Fact]
     public void PartOne()
     {
-        int result = 0;
+        int result = 1;
+
+        var times = input[0].Split(":")[1].Split(" ").Where(substr => substr != "").Select(str => int.Parse(str.Trim()));
+        var distances = input[1].Split(":")[1].Split(" ").Where(substr => substr != "").Select(str => int.Parse(str.Trim()));
+
+        var races = times.Zip(distances);
+
+        foreach (var (time, distanceToBeat) in races)
+        {
+            var winningRuns = 0;
+
+            for (var i = 0; i < time; i++)
+            {
+                var accumulatedSpeed = i;
+                var timeToTravel = time - i;
+                var distanceTraveled = accumulatedSpeed * timeToTravel;
+
+                if (distanceTraveled > distanceToBeat) winningRuns++;
+            }
+
+            result *= winningRuns;
+        }
 
         Console.WriteLine(result);
-        Assert.Equal(-1, result);
+        Assert.Equal(4568778, result);
     }
 
     [Fact]
     public void PartTwo()
     {
-        int result = 0;
+        long result = 1;
+
+        var time = long.Parse(string.Join("", input[0].Split(":")[1].Split(" ").Where(substr => substr != "")));
+        var distanceToBeat = long.Parse(string.Join("", input[1].Split(":")[1].Split(" ").Where(substr => substr != "")));
+
+
+        var winningRuns = 0;
+
+        for (var i = 0; i < time; i++)
+        {
+            var accumulatedSpeed = i;
+            var timeToTravel = time - i;
+            var distanceTraveled = accumulatedSpeed * timeToTravel;
+
+            if (distanceTraveled > distanceToBeat) winningRuns++;
+        }
+
+        result *= winningRuns;
 
         Console.WriteLine(result);
-        Assert.Equal(-1, result);
+        Assert.Equal(28973936, result);
     }
 }
