e1b0870626139d5b1b799e80f8bd8f6d07afbe96
Fredrik Haarde <fredrik.haarde@bergen.kommune.no>
2023-12-14 20:13:18 +0100

    Day 1

--- 2015/Day01/Day01.cs
+++ 2015/Day01/Day01.cs
@@ -2,23 +2,56 @@ namespace _2015.Day01;
 
 public class Day01
 {
-    private static readonly string[] input = File.ReadAllLines("../../../Day01/input.txt");
+    private static readonly string input = File.ReadAllText("../../../Day01/input.txt");
 
     [Fact]
     public void PartOne()
     {
         int result = 0;
 
+        foreach (char c in input)
+        {
+            if (c == '(')
+            {
+                result++;
+            }
+            else
+            {
+                result--;
+            }
+        }
+
         Console.WriteLine(result);
-        Assert.Equal(-1, result);
+        Assert.Equal(232, result);
     }
 
     [Fact]
     public void PartTwo()
     {
         int result = 0;
+        int count = 1;
+
+        foreach (char c in input)
+        {
+            if (c == '(')
+            {
+                result++;
+            }
+            else
+            {
+                result--;
+            }
+
+            if (result == -1)
+            {
+                result = count;
+                break;
+            }
+
+            count++;
+        }
 
         Console.WriteLine(result);
-        Assert.Equal(-1, result);
+        Assert.Equal(1783, result);
     }
 }
