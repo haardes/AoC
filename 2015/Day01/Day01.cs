3541881e91520e07b2cca0be017910f5a451540c
Fredrik Haarde <fredrik.haarde@bergen.kommune.no>
2023-12-14 14:38:56 +0100

    Init 2015

---
+++ 2015/Day01/Day01.cs
@@ -0,0 +1,24 @@
+namespace _2015.Day01;
+
+public class Day01 
+{
+    private static readonly string[] input = File.ReadAllLines("../../../Day01/input.txt");
+
+    [Fact]
+    public void PartOne()
+    {
+        int result = 0;
+
+        Console.WriteLine(result);
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void PartTwo()
+    {
+        int result = 0;
+
+        Console.WriteLine(result);
+        Assert.Equal(-1, result);
+    }
+}
