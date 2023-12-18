9e95c25727f6526d0448cb775514fe5b9903c239
haardes <fredrik.haarde@gmail.com>
2023-12-18 09:45:53 +0100

    Day 7

--- 2015/Day07/Day07.cs
+++ 2015/Day07/Day07.cs
@@ -1,77 +1,83 @@
+using Microsoft.VisualStudio.TestPlatform.CoreUtilities.Helpers;
+
 namespace _2015.Day07;
 
 public class Day07
 {
     private static readonly string[] input = File.ReadAllLines("../../../Day07/input.txt");
+    private Dictionary<string, string> commands = new();
+    private Dictionary<string, ushort> wires = new();
 
     [Fact]
     public void PartOne()
     {
         int result = 0;
-        Dictionary<string, ushort> wires = new();
 
         foreach (string line in input)
         {
-            string[] parts = line.Split(" -> ");
+            string command = line.Split(" -> ").First();
+            string targetWire = line.Split(" -> ").Last();
 
-            string wireKey = parts.Last();
+            commands.Add(targetWire, command);
+        }
 
-            if (!wires.ContainsKey(wireKey))
-            {
-                wires.Add(wireKey, 0);
-            }
+        result = GetWireValue("a");
 
-            string[] instructions = parts.First().Split(" ");
-            Console.WriteLine(line);
+        Console.WriteLine(result);
+        Assert.Equal(16076, result);
+    }
 
-            if (instructions.Length == 1)
-            {
-                try
+    private ushort GetWireValue(string wireKey)
+    {
+        if (ushort.TryParse(wireKey, out ushort value)) return value;
+        if (!commands.TryGetValue(wireKey, out string? command)) throw new Exception($"Something went wrong fetching key {wireKey}");
+        if (wires.TryGetValue(wireKey, out ushort cachedValue)) return cachedValue;
+
+        ushort result = 0;
+        string[] instructions = command.Split(" ");
+
+        switch (instructions.Length)
+        {
+            case 1:
+                string valueOrWire = instructions.First();
+                if (ushort.TryParse(valueOrWire, out ushort wireValue))
                 {
-                    wires[wireKey] = ushort.Parse(instructions.First());
+                    result = wireValue;
                 }
-                catch (FormatException)
+                else
                 {
-                    wires[wireKey] = wires.GetValueOrDefault(instructions.First());
+                    result = GetWireValue(valueOrWire);
                 }
-            }
-            else if (instructions.Length == 2)
-            {
-                wires[wireKey] = (ushort)~wires.GetValueOrDefault(instructions.Last());
-            }
-            else
-            {
-                string firstWireKey = instructions.First();
-                string instruction = instructions.ElementAt(1);
-                string lastKey = instructions.Last();
+                break;
+            case 2:
+                string wire = instructions.Last();
+                result = (ushort)~GetWireValue(wire);
+                break;
+            case 3:
+                string wireOrValue = instructions[0];
+                string instruction = instructions[1];
+                string wireOrValue2 = instructions[2];
 
                 switch (instruction)
                 {
                     case "AND":
-                        wires[wireKey] = (ushort)(wires.GetValueOrDefault(firstWireKey) & wires.GetValueOrDefault(lastKey));
+                        result = (ushort)(GetWireValue(wireOrValue) & GetWireValue(wireOrValue2));
                         break;
                     case "OR":
-                        wires[wireKey] = (ushort)(wires.GetValueOrDefault(firstWireKey) ^ wires.GetValueOrDefault(lastKey));
+                        result = (ushort)(GetWireValue(wireOrValue) ^ GetWireValue(wireOrValue2));
                         break;
                     case "LSHIFT":
-                        wires[wireKey] = (ushort)(wires.GetValueOrDefault(firstWireKey) << int.Parse(lastKey));
+                        result = (ushort)(GetWireValue(wireOrValue) << GetWireValue(wireOrValue2));
                         break;
                     case "RSHIFT":
-                        wires[wireKey] = (ushort)(wires.GetValueOrDefault(firstWireKey) >> int.Parse(lastKey));
+                        result = (ushort)(GetWireValue(wireOrValue) >> GetWireValue(wireOrValue2));
                         break;
                 }
-            }
+                break;
         }
 
-        foreach (var wire in wires)
-        {
-            Console.WriteLine(wire);
-        }
-
-        result = wires["a"];
-
-        Console.WriteLine(result);
-        Assert.Equal(-1, result);
+        wires.Add(wireKey, result);
+        return result;
     }
 
     [Fact]
@@ -79,7 +85,23 @@ public class Day07
     {
         int result = 0;
 
+        /* Now, take the signal you got on wire a, override wire b to that signal, and reset the other wires (including wire a). What new signal is ultimately provided to wire a? */
+        foreach (string line in input)
+        {
+            string command = line.Split(" -> ").First();
+            string targetWire = line.Split(" -> ").Last();
+
+            commands.Add(targetWire, command);
+        }
+
+        result = GetWireValue("a");
+        wires = new()
+        {
+            { "b", (ushort)result }
+        };
+        result = GetWireValue("a");
+
         Console.WriteLine(result);
-        Assert.Equal(-1, result);
+        Assert.Equal(2797, result);
     }
 }
