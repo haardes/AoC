88f1db3f8bececcd9af385f2497cc84ec6d26c50
Fredrik Haarde <fredrik.haarde@bergen.kommune.no>
2023-12-19 07:48:30 +0100

    Apparently stuck at day 9

--- 2015/Day09/Day09.cs
+++ 2015/Day09/Day09.cs
@@ -1,16 +1,46 @@
+
+
 namespace _2015.Day09;
 
 public class Day09
 {
     private static readonly string[] input = File.ReadAllLines("../../../Day09/test_input.txt");
+    private readonly List<NetworkNode> networkNodes = new();
 
     [Fact]
     public void PartOne()
     {
-        int result = 0;
+        int result = int.MaxValue;
+        CreateNetworkFromInput();
+        DisplayNetwork();
 
-        Dictionary<string, NetworkNode> network = new();
+        List<List<NetworkNode>> permutations = GeneratePermutations();
 
+        foreach (var permutation in permutations)
+        {
+            int cost = 0;
+            NetworkNode previous = permutation.First();
+
+            foreach (var node in permutation)
+            {
+                cost += GetDistance(previous, node);
+                previous = node;
+            }
+
+            if (cost < result) result = cost;
+        }
+
+        Console.WriteLine(result);
+        Assert.Equal(-1, result);
+    }
+
+    private static int GetDistance(NetworkNode previous, NetworkNode node)
+    {
+        return previous.Neighbours.Find(n => n.Node.Name == node.Name).Distance;
+    }
+
+    private void CreateNetworkFromInput()
+    {
         foreach (string line in input)
         {
             string[] distances = line.Split(" = ");
@@ -18,55 +48,33 @@ public class Day09
             string to = distances.First().Split(" to ").First();
             string from = distances.First().Split(" to ").Last();
 
-            if (!network.TryGetValue(to, out NetworkNode? toNode))
+            NetworkNode? toNode = networkNodes.Find(node => node.Name == to);
+            NetworkNode? fromNode = networkNodes.Find(node => node.Name == from);
+
+            if (toNode == null)
             {
                 toNode = new(to);
-                network.Add(to, toNode);
+                networkNodes.Add(toNode);
             }
 
-            if (!network.TryGetValue(from, out NetworkNode? fromNode))
+            if (fromNode == null)
             {
                 fromNode = new(from);
-                network.Add(from, fromNode);
+                networkNodes.Add(fromNode);
             }
 
-            network.GetValueOrDefault(to)!.AddNeighbour(network.GetValueOrDefault(from)!, distance);
-            network.GetValueOrDefault(from)!.AddNeighbour(network.GetValueOrDefault(to)!, distance);
+            toNode.AddNeighbour(fromNode, distance);
+            fromNode.AddNeighbour(toNode, distance);
         }
-
-        Console.WriteLine(result);
-        Assert.Equal(-1, result);
     }
 
-    public static IEnumerable<IEnumerable<T>> Permute<T>(IEnumerable<T> sequence)
+    private void DisplayNetwork()
     {
-        if (sequence == null)
+        foreach (NetworkNode node in networkNodes)
         {
-            yield break;
-        }
-
-        var list = sequence.ToList();
-
-        if (!list.Any())
-        {
-            yield return Enumerable.Empty<T>();
-        }
-        else
-        {
-            var startingElementIndex = 0;
-
-            foreach (var startingElement in list)
-            {
-                var index = startingElementIndex;
-                var remainingItems = list.Where((e, i) => i != index);
-
-                foreach (var permutationOfRemainder in Permute(remainingItems))
-                {
-                    yield return permutationOfRemainder.Prepend(startingElement);
-                }
-
-                startingElementIndex++;
-            }
+            Console.Write($"{node.Name,-10}: ");
+            node.Neighbours.ForEach(neighbour => Console.Write($"({neighbour.Node.Name,-10}, {neighbour.Distance:d3}), "));
+            Console.WriteLine();
         }
     }
 
@@ -83,7 +91,7 @@ public class Day09
 public class NetworkNode
 {
     public string Name;
-    public List<(string Node, int Distance)> Neighbours = new();
+    public List<(NetworkNode Node, int Distance)> Neighbours = new();
 
     public NetworkNode(string name)
     {
@@ -92,7 +100,7 @@ public class NetworkNode
 
     public NetworkNode AddNeighbour(NetworkNode node, int distance)
     {
-        Neighbours.Add((node.Name, distance));
+        Neighbours.Add((node, distance));
         return this;
     }
 }
