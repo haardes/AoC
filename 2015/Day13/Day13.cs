ddee81a1f0a55688d612249ac192df761da3a3d8
Fredrik Haarde <fredrik.haarde@bergen.kommune.no>
2023-12-20 12:48:02 +0100

    Init day 13, not solved

---
+++ 2015/Day13/Day13.cs
@@ -0,0 +1,26 @@
+using System.Text.RegularExpressions;
+
+namespace _2015.Day13;
+
+public class Day13
+{
+    private static readonly string[] input = File.ReadAllLines("../../../Day13/input.txt");
+
+    [Fact]
+    public void PartOne()
+    {
+        int result = 0;
+
+        Console.WriteLine(result);
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void PartTwo()
+    {
+        int result = 0;
+
+        Console.WriteLine(result);
+        Assert.Equal(-1, result);
+    }
+}
